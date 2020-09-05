14768e5f5fe3392252fb775218ea8bb19d7e5313
Edwin Xico <contact@edwinxico.com>
2020-09-05 16:02:14 -0600

    Update remainig strings where the word eCommerce was used instead of WC

--- wc-guatemala-departamentos-y-ciudades.php
+++ wc-guatemala-departamentos-y-ciudades.php
@@ -7,7 +7,7 @@
  * Author URI: https://coders.club.gt
  * License: GNU General Public License v3.0
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
- * Text Domain: dl-ecommerce-guatemala
+ * Text Domain: wc-guatemala
  * Domain Path: /languages
  * WC tested up to: 4.0
  * WC requires at least: 3.6
